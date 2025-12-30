dca05af5d0226a980d0de490f2d9f114712d731e
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2025-12-30 23:34:22 +0530

    ginal changes

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://mini-user-management-system-production-87fa.up.railway.app",
+ withCredentials: true,
 });
+
 
 export default api;
