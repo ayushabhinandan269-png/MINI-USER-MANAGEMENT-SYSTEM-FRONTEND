74925ca002f8770464a6ebf3547b608a5466dd5b
ayushabhinandan269-png <ayushabhinandan269@gmail.com>
2025-12-31 01:49:35 +0530

    final modified codes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mini-user-management-system-production-87fa.up.railway.app",
+  baseURL: "mini-user-management-system-backend-production.up.railway.app",
  withCredentials: true,
 });
 
